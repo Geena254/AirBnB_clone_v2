b958106116193845b4b0ae9c9b61a0165ba0a679
Joeeazy <joeeazy34@gmail.com>
2024-01-17 09:21:41 +0300

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models import storage
 from datetime import datetime
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Interger, String, DateTime
+
+if models.storage_type == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    if models.storage_type == "db":
+        id = Column(String(60), unique=True, nullable=False, primary_key=True,
+                    default=str(uuid4()))
+        created_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -25,20 +42,34 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        my_dict = {
+                key: value
+                for key, value in self.__dict__.items()
+                if key != "_sa_instance_state"
+                }
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
+        dictionary = {
+            key: value
+            for key, value in self.__dict__.items()
+            if key != "_sa_instance_state"
+        }
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        
         return dictionary
+
+    def delete(self):
+        "Deletes slef from the storage"
+        storage.delete(self)
