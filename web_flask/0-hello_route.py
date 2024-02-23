2899b77e92ab0ef313758ff5f41fc665e7179cd2
Georgina Kimani <njokikimani001@gmail.com>
2024-02-23 18:27:12 +0000

    flask web framework

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that strats a Flask web application."""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Route to display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    # Run the app on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
